c57391c6854d816904f1aaf5b7ea1a140eed9688
Chris A <autrey@gmail.com>
2016-07-18 14:56:23 -0500

    C# Library for Another Task App

---
+++ atapp/Task.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atapp
+{
+    public class Task
+    {
+        public string Name { get; set; }
+    }
+}
